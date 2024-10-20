e80d0f504dd50578c757599df1f293cf6dba82c9
xbb4u <118782227+xbb4u@users.noreply.github.com>
2024-10-20 07:54:21 +0100

    Update players.lua

--- server/storage/players.lua
+++ server/storage/players.lua
@@ -1,6 +1,6 @@
 local defaultSpawn = require 'config.shared'.defaultSpawn
 local characterDataTables = require 'config.server'.characterDataTables
-
+--test
 ---@param request InsertBanRequest
 ---@return boolean success
 ---@return ErrorResult? errorResult
